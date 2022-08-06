88341fdc96d52b5cf96bde8f1106c14be1551e52
westhide <mr.westhide@gmail.com>
2022-08-07 00:13:33 +0800

    chore: add vue type declare

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,18 @@
+/// <reference types="vue/macros-global" />
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vitest/importMeta" />
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    unknown
+  >;
+  export default component;
+}
 
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
