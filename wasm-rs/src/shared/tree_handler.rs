bd708975cc7790851a1df26f1e139a2cf8add266
westhide <mr.westhide@gmail.com>
2022-09-05 07:03:46 +0800

    wip(wasm-rs): TreeHandler

--- wasm-rs/src/shared/tree_handler.rs
+++ wasm-rs/src/shared/tree_handler.rs
@@ -1,11 +1,18 @@
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
-use crate::utils::tree::{handler::TreeHandler, ops::AddChild, Tree};
+use crate::utils::tree::{
+    handler::TreeHandler,
+    id::{Identity, ID},
+    rc_tree::{RcNodeTree, RcTree},
+    store::Store,
+    Tree,
+};
 
 #[wasm_bindgen(typescript_custom_section)]
 const Ts_Tree: &'static str = r#"
 type TreeOptions = {
-  node: string;
+  node: {id:string,label?:string};
   info: string;
   children?: TreeOptions[];
 };
@@ -17,15 +24,41 @@ extern "C" {
     pub type TsTreeOptions;
 }
 
+#[derive(Debug, Serialize, Deserialize, Default)]
+struct TreeItem {
+    id: ID,
+    label: Option<String>,
+}
+
+impl Identity for TreeItem {
+    fn id(&self) -> ID {
+        self.id.clone()
+    }
+}
+
 #[wasm_bindgen(js_name = "treeHandler")]
 pub fn tree_handler(options: TsTreeOptions) {
-    let raw_tree: Tree<String, String> = options.into_serde().unwrap();
-    let mut handler = TreeHandler::new(raw_tree);
-    let rc_tree = TreeHandler::rc_tree(Tree::new(
-        "5".to_string(),
-        "1-5".to_string(),
-        None,
-    ));
-    handler.update_tree().add_child(rc_tree);
-    web_sys::console::log_1(&(format!("{handler:#?}")).into());
+    let raw_tree: Tree<TreeItem, String> = options.into_serde().unwrap();
+    let rc_tree = RcTree::<TreeItem, String>::from(raw_tree);
+
+    let tree_json = serde_json::to_string_pretty(&rc_tree).unwrap();
+    web_sys::console::log_1(&(format!("{tree_json}")).into());
+
+    let handler = TreeHandler::from(rc_tree);
+
+    let keys = handler.store.nodes();
+    web_sys::console::log_1(&(format!("{keys:#?}")).into());
+
+    let iter: Vec<_> = handler
+        .tree
+        .iter()
+        .map(|cursor| cursor.borrow().node.clone())
+        .collect();
+    web_sys::console::log_1(&(format!("{iter:#?}")).into());
+
+    // let key = keys[1];
+    // let sibling = handler.store.child_nodes(key);
+    //
+    // let json = serde_json::to_string_pretty(&sibling).unwrap();
+    // web_sys::console::log_1(&(format!("{key}{json}")).into());
 }
